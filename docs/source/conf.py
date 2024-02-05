e3ee01b277ca0e101a046b3bccb5a6441943a0ee
ctuni <c.tuni.dominguez@gmail.com>
2024-02-05 15:14:45 +0100

    re-added requirements and theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_css_files = ["custom.css"]
 html_logo = 'flomics_icon_with_logo_Image.png'
